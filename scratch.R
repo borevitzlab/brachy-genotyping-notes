44a3c98ad2460665afa5f9ab337f94745f203524
Kevin Murray <kdmfoss@gmail.com>
2017-12-14 16:06:13 +1100

    Final structure run z=2.5

--- scratch.R
+++ scratch.R
@@ -12,3 +12,22 @@ ggplot(genocall, aes(group, missing.rate)) +
     geom_violin(scale="count")
 
 ?geom_violin
+
+library(tidyverse)
+dodgy = read.csv("data/dodgy-samples.csv")$anon.name
+readstats = read.delim("data/2017-12-12_br-readstats.tsv") %>% 
+    mutate(anon.name = sub('^.+/(BR\\d\\d[A-H]\\d\\d).*', '\\1', filename)) %>% 
+    select(-filename) %>% 
+    filter(anon.name %in% filt.dis$samps) %>% 
+    mutate(dodgy = ifelse(anon.name %in% dodgy, "FAIL", "PASS"),
+           missing = filt.dis$miss.samp[match(filt.dis$samps, anon.name)])
+str(readstats)
+
+ggplot(readstats, aes(x=dodgy, y=bases)) +
+    geom_violin(scale="count")
+
+ggplot(readstats, aes(x=dodgy, y=missing)) +
+    geom_violin(scale="count")
+
+t.test(bases ~ dodgy, data=readstats)
+t.test(missing ~ dodgy, data=readstats)
